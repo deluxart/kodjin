6bbb34d887fcbd6a15a5ff0577084cd739b33ef0
AlexanderOsadchyy <alex.o@blaize.tech>
2024-10-25 02:14:33 +0300

    Fix bugs

--- page-onc-compliance-solution.php
+++ page-onc-compliance-solution.php
@@ -176,7 +176,7 @@ $section_bg_image = get_field( 'section_bg_image' );
                             </div>
                             <?php $link = get_sub_field( 'link' ); ?>
                             <?php if ( $link ) : ?>
-                                <a href="<?php echo esc_url( $link) ; ?>" class="read">
+                                <a href="<?php echo esc_url( $link) ; ?>" target="_blank" class="read">
                                     <svg width="100%" height="100%" viewBox="0 0 23 23" fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <path fill-rule="evenodd" clip-rule="evenodd" d="M22.2544 1.89649L22.3988 0.134546L20.6369 0.278968L8.8555 1.24465C8.02984 1.31233 7.41537 2.03652 7.48305 2.86218C7.55073 3.68784 8.27492 4.3023 9.10058 4.23463L16.8085 3.60283L0.672229 19.7391C0.086442 20.3249 0.0864427 21.2746 0.672229 21.8604C1.25802 22.4462 2.20776 22.4462 2.79355 21.8604L18.9307 5.72326L18.2987 13.4328C18.2311 14.2585 18.8455 14.9826 19.6712 15.0503C20.4969 15.118 21.221 14.5035 21.2887 13.6779L22.2544 1.89649Z" fill="currentColor"/>
                                     </svg>
@@ -227,7 +227,7 @@ $section_bg_image = get_field( 'section_bg_image' );
              data-aos-delay="100">
             <div class="container">
                     <div class="wrap_btn">
-                        <a href="#" class="btn dark">Contact Experts</a>
+                        <button data-name="request_demo" class="btn dark da-modal">Contact Experts</button>
                     </div>
             </div>
         </div>
@@ -310,7 +310,7 @@ $section_bg_image = get_field( 'section_bg_image' );
                         </div>
                         <h2>Download Kodjin Interoperability Suite</h2>
                         <div class="actions">
-                            <a href="<?php echo get_home_url(); ?>/whitepapers/the-ultimate-guide-to-fhir/" class="btn white">Download</a>
+                            <a href="<?php echo get_home_url(); ?>/whitepapers/fhir-based-enterprise-level-tools-for-healthcare-data-management/" class="btn white">Download</a>
                         </div
                     </div>
                 </section>
@@ -408,9 +408,7 @@ $section_bg_image = get_field( 'section_bg_image' );
                                         <?php if ( get_sub_field( 'add_button' ) == 1 ) : ?>
                                         <?php if ( have_rows( 'button' ) ) : ?>
                                             <?php while ( have_rows( 'button' ) ) : the_row(); ?>
-
-                                                <a href="<?php the_sub_field( 'link' ); ?>" class="btn dark border"><?php the_sub_field( 'title' ); ?></a>
-
+                                                    <button data-name="request_demo" class="btn dark border da-modal"><?php the_sub_field( 'title' ); ?></button>
                                             <?php endwhile; ?>
                                         <?php endif; ?>
                                     <?php endif; ?>
@@ -447,29 +445,45 @@ $section_bg_image = get_field( 'section_bg_image' );
 
 
 <!--                    03-->
-                    <?php if ( have_rows( 'section_roadmap_03' ) ) : $second_list_li = 0; ?>
-                    <div class="block_section">
-                        <div class="block_num">03</div>
-                        <?php while ( have_rows( 'section_roadmap_03' ) ) : the_row(); ?>
-                            <h3><?php the_sub_field( 'title' ); ?></h3>
-                        <div class="second_list">
-                            <?php if ( have_rows( 'list' ) ) : ?>
-                                <?php while ( have_rows( 'list' ) ) : the_row(); $second_list_li++; ?>
-                                <div class="item">
-                                    <div class="num">
-                                        <?php echo $second_list_li; ?>
-                                    </div>
-                                    <div>
-                                        <?php the_sub_field( 'item' ); ?>
-                                    </div>
+                    <?php if ( have_rows( 'section_roadmap_03' ) ) : ?>
+                        <div class="block_section">
+                            <div class="block_num">03</div>
+                            <?php while ( have_rows( 'section_roadmap_03' ) ) : the_row(); ?>
+                                <h3><?php the_sub_field( 'title' ); ?></h3>
+                                <div class="second_list">
+                                    <?php
+                                    // Собираем все элементы в массив
+                                    $items = [];
+                                    if ( have_rows( 'list' ) ) :
+                                        while ( have_rows( 'list' ) ) : the_row();
+                                            $items[] = get_sub_field( 'item' );
+                                        endwhile;
+                                    endif;
+
+                                    // Количество элементов
+                                    $count = count($items);
+                                    $half = ceil($count / 2); // Половина массива
+
+                                    // Выводим элементы в правильном порядке, но с обновленной нумерацией
+                                    for ($i = 0; $i < $count; $i++) :
+                                        // Нумерация по строкам: 1, 2, 3... для первой колонки, 4, 5, 6... для второй
+                                        $number = ($i % 2 === 0)
+                                            ? floor($i / 2) + 1 // Первая колонка
+                                            : floor($i / 2) + 1 + $half; // Вторая колонка
+                                        ?>
+                                        <div class="item">
+                                            <div class="num"><?php echo $number; ?></div> <!-- Правильная нумерация -->
+                                            <div><?php echo $items[$i]; ?></div> <!-- Пункт остаётся на месте -->
+                                        </div>
+                                    <?php endfor; ?>
                                 </div>
-                                <?php endwhile; ?>
-                            <?php endif; ?>
+                            <?php endwhile; ?>
                         </div>
-                        <?php endwhile; ?>
-                    </div>
                     <?php endif; ?>
-<!--                    03-->
+
+
+
+                    <!--                    03-->
                 </div>
                 <?php if ( have_rows( 'actions_roadmap' ) ) : $a_is_card = 1; ?>
                     <div class="cards actions">
